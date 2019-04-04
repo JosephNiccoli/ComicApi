21c65a446b50c7a899b5f4f52f68baa5a78c4374
Joseph Niccoli <niccoli.joseph592@outlook.com>
2019-04-03 17:07:46 -0700

    first commit

---
+++ ComicApi/Controllers/ComicController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ComicApi.Controllers
+{
+	[] //route and route prefix this is one of the first thing to think about in a new controller
+    public class ComicController : ApiController
+    {
+    }
+}
