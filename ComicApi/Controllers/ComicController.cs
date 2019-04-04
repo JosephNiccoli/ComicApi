b53798a221157b5d45e92bad045f7c91f7014e4a
Joseph Niccoli <niccoli.joseph592@outlook.com>
2019-04-03 18:35:22 -0700

    Creating the Service

--- ComicApi/Controllers/ComicController.cs
+++ ComicApi/Controllers/ComicController.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.Remoting.Messaging;
 using System.Web.Http;
 
 namespace ComicApi.Controllers
 {
-	[] //route and route prefix this is one of the first thing to think about in a new controller
+	[RoutePrefix("api/comic")] //route and route prefix this is one of the first thing to think about in a new controller
     public class ComicController : ApiController
     {
+		// the job of a controller is to get the work over to a service and the service will do all the actual work
+
+		// create a method
+		public object GetAllComics() 
+		{
+
+		}
+
+	 
     }
 }
