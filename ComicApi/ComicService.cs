bc8c5283d1a456c3b06db93d8d594b2387474bd0
Joseph Niccoli <niccoli.joseph592@outlook.com>
2019-04-03 20:49:10 -0700

    Creating a model

--- ComicApi/ComicService.cs
+++ ComicApi/ComicService.cs
@@ -8,6 +8,13 @@ namespace ComicApi
 	public class ComicService
 	{
 		// this needs to get all the people
+		public object GetAllComics()
+		{
+			// ADO.NET goes in here
 
+			//needs a model a class that holds data
+
+
+		}
 	}
 }
