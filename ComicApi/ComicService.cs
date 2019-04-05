440d69a7f6849c0969ffb70dff057e6ae2f5d46e
Joseph Niccoli <niccoli.joseph592@outlook.com>
2019-04-04 21:09:59 -0700

    finished Comic properties starting on the ADO in the ComicService

--- ComicApi/ComicService.cs
+++ ComicApi/ComicService.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ComicApi.Models;
 
 namespace ComicApi
 {
 	public class ComicService
 	{
 		// this needs to get all the people
-		public object GetAllComics()
+		//public object GetAllComics()
+		//{
+		//	// ADO.NET goes in here
+
+		//	//needs a model a class that holds data
+		//}
+		// now that the model is created you can be more specific with the method so it should be public List<Comic> GetAllComics(){}
+
+		public List<Comic> GetAllComics()
 		{
 			// ADO.NET goes in here
-
-			//needs a model a class that holds data
 
 
 		}
--- ComicApi/Models/Comic.cs
+++ ComicApi/Models/Comic.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Web;
 
+
+// this is just a holder for some data
 namespace ComicApi.Models
 {
 	public class Comic
 	{
-
+		//id, comic_name, comic_company, comic_style, introduction
+		public int Id { get; set; }
+		public string ComicName { get; set; }
+		public string ComicCompany { get; set; }
+		public string ComicStyle { get; set; }	
+		public string Introduction { get; set; }	
 	}
 }
