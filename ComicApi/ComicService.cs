bbf9593c51e3a85b6e331826923eec3b96cbafb4
Joseph Niccoli <niccoli.joseph592@outlook.com>
2019-04-05 18:01:04 -0700

    updated comment for the list loop in the db for ther IsDbNull

--- ComicApi/ComicService.cs
+++ ComicApi/ComicService.cs
@@ -47,7 +47,7 @@ namespace ComicApi
 						comic.Id = dr.GetInt32(0);
 						comic.ComicName = dr.GetString(1);
 						comic.ComicCompany = dr.GetString(2);
-						//if (!dr.IsDBNull(3)) // if u want a nullable value you have to do it like this
+						//if (!dr.isDBNull(3)) comic.ComicStyle = dr.GetString(3); // if u want a nullable value you have to do it like this
 						comic.ComicStyle = dr.GetString(3);
 						comic.Introduction = dr.GetString(4);
 						// and then add that Comic to the list
